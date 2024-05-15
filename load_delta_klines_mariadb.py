cc057a0e26523c346ee67af73967616e9c426b4e
Sebastien Hoffmann <aps.hoffmann@gmail.com>
2024-05-15 15:36:02 +0200

    MAJ des fichiers pour MariaDB

--- load_delta_klines_mariadb.py
+++ load_delta_klines_mariadb.py
@@ -1,31 +1,55 @@
 import pandas as pd
 from binance.client import Client
-from sqlalchemy import create_engine
-from datetime import datetime
-from historical_data import clean_data
-
-#------------------------------------------------------------------------------------------------
-
-# Define which symbol data market constantly add to the database
-symbol = 'BTCEUR'
-
-# Define Client and download data
-klinesT = Client().get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1HOUR)
-df = pd.DataFrame(klinesT, columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
-
-# Only keeping the last 24h data
-df = df.tail(24)
-
-# DF cleaning
-clean_data(df)
-
-#Test
-#print(df)
+from sqlalchemy import create_engine, text
 
 # Check if data already exists in database before loading it
-def check_existing_data(engine, symbol, date):
+
+# Define the connection URL
+DB_USERNAME = 'user'
+DB_PASSWORD = 'secretpw'
+DB_HOST = 'localhost'
+DB_PORT = '3306'
+DB_NAME = 'ProjetOPA'
+
+# Create the connection URL
+connection_url = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+# Create the database engine
+engine = create_engine(connection_url)
+
+# Create connection
+connection = engine.connect()
+
+def check_existing_data(symbol, date):
     """Check if data already exists in the database."""
-    query = f"SELECT COUNT(*) FROM data_market WHERE symbol = '{symbol}' AND timestamp >= '{date}'"
-    result = engine.execute(query)
+    query = f"SELECT COUNT(*) FROM data_market WHERE (symbol = '{symbol}') AND (open_time = '{date}')"
+    result = connection.execute(text(query))
     count = result.fetchone()[0]
     return count > 0
+
+# Convert the crypto_histo.json file to a DataFrame
+df = pd.read_json('crypto_histo.json', orient = 'split', compression = 'infer')
+
+def load_data(df, db_url, table):
+    """Load the DataFrame DF in the table, if each row of the DF doesn't already exists in the table
+
+    Args:
+        df (DataFrame): DataFrame to load to the table
+        db_url (str): url of the database
+        table (str): table name
+    """
+    for index in range(len(df)):
+        row = df.iloc[index]
+        if check_existing_data(row['symbol'], row['open_time']): 
+            print(f"Data already exists in {table} for symbol {row['symbol']} and open_time {row['open_time']}.")
+        else:     
+            # Extract the row as a DF
+            row_df = pd.DataFrame([row])
+            # Load the row in the table
+            row_df.to_sql(name=table, con=engine, if_exists='append', index=False)
+            print(f"Data for symbol {row['symbol']} and open_time {row['open_time']} successfully loaded into {table}.")
+
+load_data(df, connection_url, 'data_market')
+
+connection.close()
+
--- load_histo_klines_mariadb.py
+++ load_histo_klines_mariadb.py
@@ -1,128 +1,28 @@
+import json
 import pandas as pd
-from binance.client import Client
-from sqlalchemy import create_engine
-import pymysql
+from sqlalchemy import create_engine, text
 
-#------------------------------------------------------------------------------------------------
-# CREATE & CLEAN DF
+# Define the connection URL
+DB_USERNAME = 'user'
+DB_PASSWORD = 'secretpw'
+DB_HOST = 'localhost'
+DB_PORT = '3306'
+DB_NAME = 'ProjetOPA'
 
-# Define which symbol data market add to the database and since when
-symbol = 'BTCEUR'
-start_date = "01 January 2022"
+# Create the connection URL
+connection_url = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-def get_data(symbol, start_date):
-    """Get data from Binance.
+# Create the database engine
+engine = create_engine(connection_url)
 
-    Args:
-        symbol (str): Symbol wanted from Binance (ex: BTCEUR, ETHUSD ...)
-        start_date (str): Date the data has been collected since (ex: '01 January 2022')
+# Connect to the database
+connection = engine.connect()
 
-    Returns:
-        DataFrame: DF containing market data of the symbol since the input start_date
-    """
-    klinesT = Client().get_historical_klines(symbol, Client.KLINE_INTERVAL_1HOUR, start_date)
-    df = pd.DataFrame(klinesT, columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
-    Client().close_connection
-    return df
+#Convert json file to DataFrame
+df = pd.read_json('crypto_histo.json', orient = 'split', compression = 'infer')
 
-#get data from Binance
-df = get_data(symbol, start_date)
+# Write the DataFrame to the database
+df.to_sql('data_market', con=engine, if_exists='replace', index=False)
 
-
-def clean_data(df):
-    """Clean a dataframe from get_historical_klines or get_klines method, only keeping timestamp(datetime), symbol(str), open(float), high(float), low(float), close(float) and volume(float).
-
-    Args:
-        df (DataFrame): DataFrame to clean
-    """
-    # Clean dataset
-    del df['ignore']
-    del df['close_time']
-    del df['quote_av']
-    del df['trades']
-    del df['tb_base_av']
-    del df['tb_quote_av']
-
-    # Add symbol column
-    df.insert(1, 'symbol', str(symbol))
-
-    # Convert to numeric
-    df['close'] = pd.to_numeric(df['close'])
-    df['high'] = pd.to_numeric(df['high'])
-    df['low'] = pd.to_numeric(df['low'])
-    df['open'] = pd.to_numeric(df['open'])
-    df['volume'] = pd.to_numeric(df['volume'])
-
-    # Convert time (ms) to datetime
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-
-#DF cleaning
-clean_data(df)
-
-#------------------------------------------------------------------------------------------------
-# LOAD DATA IN SQL DATABASE
-
-# Database settings
-username = 'user'
-password = 'secretpw'
-hostname = 'localhost'
-port = '3306'
-database = 'ProjetOPA'
-table = 'data_market'
-
-db_url = f'mysql://{username}:{password}@{hostname}:{port}/{database}'
-
-# Create engine
-engine = create_engine(db_url)
-
-df.to_sql(name=table, con=engine, if_exists='replace', index=False)
-
-try:
-    connection = engine.connect()
-    print("Connection successful!")
-    connection.close()
-except Exception as e:
-    print("Connection failed:", e)
-
-'''
-def check_existing_data(engine, table, symbol, timestamp):
-    """Check if data already exists in the specified table for a given symbol and timestamp."""
-    with engine.connect() as conn:
-        # Construire la requête SQL pour vérifier si les données existent déjà
-        query = f"SELECT COUNT(*) FROM {table} WHERE symbol = '{symbol}' AND timestamp = '{timestamp}'"
-        
-        # Exécuter la requête SQL
-        result = conn.execute(query)
-        
-        # Récupérer le résultat
-        count = result.fetchone()[0]
-        
-        # Si le nombre de lignes retourné est supérieur à zéro, les données existent déjà
-        return count > 0
-'''
-
-
-
-# Load data in database
-'''
-def load_data(df, db_url, table):
-    """Load the DataFrame DF in the table, if each row of the DF doesn't already exists in the table
-
-    Args:
-        df (DataFrame): DataFrame to load to the table
-        db_url (str): url of the database
-        table (str): table name
-    """
-    for index in range(len(df)):
-        row = df.iloc[index]
-        if check_existing_data(engine, table, row['symbol'], row['timestamp']): 
-            print(f"Data already exists in {table} for symbol {row['symbol']} and timestamp {row['timestamp']}.")
-        else:     
-            # Extract the row as a DF
-            row_df = pd.DataFrame([row])
-            # Load the row in the table
-            row_df.to_sql(name=table, con=engine, if_exists='append', index=False)
-            print(f"Data for symbol {row['symbol']} and timestamp {row['timestamp']} successfully loaded into {table}.")
-'''
-
-#load_data(df, db_url, table)
+# Close the connection
+connection.close()
